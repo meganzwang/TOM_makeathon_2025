0023215e5240232e31d87a93a2d8518fd1bc626f
Lisa Hernandez <lh919521@gmail.com>
2025-10-25 16:08:11 -0500

    file

--- app/src/components/BottomBar.tsx
+++ app/src/components/BottomBar.tsx
@@ -14,21 +14,55 @@ function speak(text: string) {
 
 export default function BottomBar({ onYes, onHelp, onNo }: Props) {
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-30 h-16 pb-[var(--safe-bottom)] bg-black/0">
-      <div className="h-full flex items-center justify-center gap-3">
-        <LowButton label="Yes" onClick={() => (onYes?.(), speak("Yes"))} />
-        <LowButton label="Help" onClick={() => (onHelp?.(), speak("Help"))} />
-        <LowButton label="No" onClick={() => (onNo?.(), speak("No"))} />
+    <div className="bg-transparent pb-[var(--safe-bottom)] flex justify-center">
+      <div className="flex items-center justify-center gap-6 py-6">
+        <LowButton 
+        label="Yes" 
+        color="green"
+        onClick={() => (onYes?.(), speak("Yes"))} />
+        <LowButton 
+        label="Help!!"
+        color="dark gray" 
+        onClick={() => (onHelp?.(), speak("Help"))} />
+        <LowButton 
+        label="No" 
+        color="red"
+        onClick={() => (onNo?.(), speak("No"))} />
       </div>
     </div>
   );
 }
 
-function LowButton({ label, onClick }: { label: string; onClick: () => void }) {
+function LowButton({ label, onClick, color }: { label: string; onClick: () => void; color: "green" | "red" | "dark gray";}) {
+
+  const bgColor = 
+    color === "green"
+    ? "#13ba50"
+    : color === "red"
+    ? "#d60f0f"
+    : "#363e4a";
+
+
   return (
     <button
-      className="rounded-full bg-brand/80 text-white px-4 py-2 shadow-sm border border-white/20 hover:bg-brand"
-      onClick={onClick}
+    onClick={onClick}
+    className="
+    text-white
+    rounded-[5px] px-32 py-100
+    front-poppins font-bold
+    hover: opacity-90 active:scale-95
+    tranition-transform shadow-sm
+    "
+      //rounded-full bg-brand/80 text-white px-4 py-2 shadow-sm border border-white/20 hover:bg-brand"
+      style={{
+        minWidth: "175px",
+        minHeight: "90px",
+        backgroundColor: bgColor,
+        border: `2px solid ${bgColor}`,
+        color: "#ffffff",
+        fontSize: "48px",
+        lineHeight: "48px"
+      }}
     >
       {label}
     </button>
